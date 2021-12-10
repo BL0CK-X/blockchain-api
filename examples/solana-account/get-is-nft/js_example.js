8d1927396a0b540f53b3eea32414af9a9e1ec81a
Josh Wolff <jw1@stanford.edu>
2021-12-10 12:00:43 -0800

    Add is_nft and is_candy_machine examples

--- examples/solana-account/get-is-nft/js_example.js
+++ examples/solana-account/get-is-nft/js_example.js
@@ -5,22 +5,40 @@ let defaultClient = theblockchainapi.ApiClient.instance;
 // Get a free API Key Pair: https://dashboard.theblockchainapi.com/api-keys
 
 let APIKeyID = defaultClient.authentications['APIKeyID'];
-APIKeyID.apiKey = 'API-KEY-ID';
+APIKeyID.apiKey = null;
 
 let APISecretKey = defaultClient.authentications['APISecretKey'];
-APISecretKey.apiKey = 'API-SECRET-KEY';
+APISecretKey.apiKey = null;
 
-let apiInstance = new theblockchainapi.SolanaAccountApi();
+const get_is_nft = async (public_key, network, title) =>  {
+    let apiInstance = new theblockchainapi.SolanaAccountApi();
 
-let network = "mainnet-beta"; // String | The network ID (devnet, mainnet-beta)
-let publicKey = "EEr5yQpNXf7Bru6Rt5podx56HGW9CEehXqgRGh2wa71w"; // String | The public key of the account
+    const result = await apiInstance.solanaGetAccountIsNFT(network, public_key).then((data) => {
+       return data['is_nft'];
+     }, (error) => {
+       console.error(error);
+       return null;
+     });
 
-const account_info = await apiInstance.solanaGetAccount(network, publicKey).then((data) => {
-  console.log('API called successfully.');
-  return data;
-}, (error) => {
-  console.error(error);
-  return null;
-});
+     console.log(title, "Is NFT?", result)
+     console.log("----------------------------------")
+}
 
-console.log("Account Info: ", account_info);
+
+// Get an NFT
+await get_is_nft("EEr5yQpNXf7Bru6Rt5podx56HGW9CEehXqgRGh2wa71w", 'mainnet-beta', "NFT Account")
+
+// Get an SPL Token
+await get_is_nft("SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt", 'mainnet-beta', "SPL Token")
+
+// Get smart contract
+await get_is_nft("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA", 'mainnet-beta', "Smart Contract")
+
+// Get an associated token account
+await get_is_nft("3CdXPzAgY72HnaReerSXhfaJa5WiBj6ftgsTB9sv6RKJ", 'mainnet-beta', "Associated Token Account")
+
+// Get candy machine account
+await get_is_nft("9htmDvW58pjCMQdjFbovo8cGBZviDfeP3j7DKnikHEy5", 'mainnet-beta', "Candy Machine Account")
+
+// Get a system program (wallet account, such as the public key to your wallet)
+await get_is_nft("31LKs39pjT5oj6fWjC3F76dHWf9489asCthmgj8wu7pj", 'mainnet-beta', "System Program")
--- examples/solana-account/get-is-nft/python_example.py
+++ examples/solana-account/get-is-nft/python_example.py
@@ -1,8 +1,9 @@
 from theblockchainapi import TheBlockchainAPIResource, SolanaNetwork
 
 # Get an API key pair for free here: https://dashboard.theblockchainapi.com/
-MY_API_KEY_ID = "0qiMmf8BEZYCN4E"
-MY_API_SECRET_KEY = "99t1AuOhcZHxcRQ"
+MY_API_KEY_ID = None
+MY_API_SECRET_KEY = None
+
 BLOCKCHAIN_API_RESOURCE = TheBlockchainAPIResource(
     api_key_id=MY_API_KEY_ID,
     api_secret_key=MY_API_SECRET_KEY
@@ -16,33 +17,32 @@ def example():
     except AssertionError:
         raise Exception("Fill in your key ID pair!")
 
-    def get_account(pub_key, title):
-        response = BLOCKCHAIN_API_RESOURCE.get_account_info(
+    def get_is_nft(pub_key, title, network):
+        response = BLOCKCHAIN_API_RESOURCE.get_is_nft(
             public_key=pub_key,
-            network=SolanaNetwork.MAINNET_BETA
+            network=network
         )
-        print("-" * 40)
         print(title)
         print(response)
-        print("\n\n\n")
+        print("-" * 40)
 
     # Get an NFT
-    get_account("EEr5yQpNXf7Bru6Rt5podx56HGW9CEehXqgRGh2wa71w", "NFT Account")
+    get_is_nft("EEr5yQpNXf7Bru6Rt5podx56HGW9CEehXqgRGh2wa71w", "NFT Account", SolanaNetwork.MAINNET_BETA)
 
     # Get an SPL Token
-    get_account("SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt", "SPL Token")
+    get_is_nft("SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt", "SPL Token", SolanaNetwork.MAINNET_BETA)
 
     # Get smart contract
-    get_account("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA", "Smart Contract")
+    get_is_nft("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA", "Smart Contract", SolanaNetwork.MAINNET_BETA)
 
     # Get an associated token account
-    get_account("3CdXPzAgY72HnaReerSXhfaJa5WiBj6ftgsTB9sv6RKJ", "Associated Token Account")
+    get_is_nft("3CdXPzAgY72HnaReerSXhfaJa5WiBj6ftgsTB9sv6RKJ", "Associated Token Account", SolanaNetwork.MAINNET_BETA)
 
     # Get candy machine account
-    get_account("9htmDvW58pjCMQdjFbovo8cGBZviDfeP3j7DKnikHEy5", "Candy Machine Account")
+    get_is_nft("9htmDvW58pjCMQdjFbovo8cGBZviDfeP3j7DKnikHEy5", "Candy Machine Account", SolanaNetwork.MAINNET_BETA)
 
     # Get a system program (wallet account, such as the public key to your wallet)
-    get_account("31LKs39pjT5oj6fWjC3F76dHWf9489asCthmgj8wu7pj", "System Program")
+    get_is_nft("31LKs39pjT5oj6fWjC3F76dHWf9489asCthmgj8wu7pj", "System Program", SolanaNetwork.MAINNET_BETA)
 
 
 if __name__ == '__main__':
