a7054b1effbe842fad7f2259688350719d60395e
Josh Wolff <jw1@stanford.edu>
2022-01-21 09:37:30 -0800

    Update

--- examples/tutorials/nft_collections/pull_solana_nft_collection_data.py
+++ examples/tutorials/nft_collections/pull_solana_nft_collection_data.py
@@ -16,20 +16,29 @@ def pull_mint_addresses():
     update_authorities = defaultdict(int)
 
     nfts = BLOCKCHAIN_API_RESOURCE.search_nfts(
-        # update_authority="CvywfuSVev6da9LngjJkFd2y3utrU2W9zvHw8TAmfo9P",
-        # symbol="YC\x00",
-        # symbol_search_method=SearchMethod.EXACT_MATCH,
-        nft_name="Solana Money Boy",
+        update_authority='3wezwrg4WtZumXhMNbdR6aJLSfBxdggnLzRQJUEzAZUJ',
+        nft_name="0euvre-Custom-Wave1 #",
         nft_name_search_method=SearchMethod.BEGINS_WITH,
         network=SolanaNetwork.MAINNET_BETA
     )
+
+    # nfts = BLOCKCHAIN_API_RESOURCE.search_nfts(
+    #     # update_authority="CvywfuSVev6da9LngjJkFd2y3utrU2W9zvHw8TAmfo9P",
+    #     # symbol="YC\x00",
+    #     # symbol_search_method=SearchMethod.EXACT_MATCH,
+    #     nft_name="Solana Money Boy",
+    #     nft_name_search_method=SearchMethod.BEGINS_WITH,
+    #     network=SolanaNetwork.MAINNET_BETA
+    # )
+    print(len(nfts))
+
     for nft in nfts:
         update_authorities[nft['nft_metadata']['update_authority']] += 1
 
     # We know there are only max 4 update authorities.
 
     valid_update_authorities = set()
-    min_associated_nfts_to_be_valid = 100
+    min_associated_nfts_to_be_valid = 50
 
     for update_authority, n_associated_nfts in update_authorities.items():
         if n_associated_nfts > min_associated_nfts_to_be_valid:
@@ -44,7 +53,7 @@ def pull_mint_addresses():
         if update_authority in valid_update_authorities:
             valid_nft_mint_addresses.add(mint_address)
 
-    with open("smb_mint_addresses.json", "w+") as fp:
+    with open("0euvre_mint_addresses.json", "w+") as fp:
         fp.write(json.dumps(list(valid_nft_mint_addresses)))
 
 
@@ -53,13 +62,14 @@ def pull_mint_stats():
     owners = defaultdict(int)
 
     with open("smb_mint_addresses.json", "rb+") as fp:
+        # with open("0euvre_mint_addresses.json", "rb+") as fp:
         mint_addresses = json.loads(fp.read())
 
     executor = ThreadPoolExecutor(max_workers=200)
     tasks = list()
     n_mints = len(mint_addresses)
     prices = list()
-    exchange_listings = defaultdict(int)
+    exchange_listings = defaultdict(set)
 
     for i in range(n_mints):
 
@@ -76,7 +86,7 @@ def pull_mint_stats():
                     if 'seller' in listing:
                         owner_ = listing['seller']
                     prices.append(listing['price'])
-                    exchange_listings[listing['exchange_readable']] += 1
+                    exchange_listings[listing['exchange_readable']].add(mint_address)
                 except Exception as _:
                     pass
 
@@ -111,8 +121,27 @@ def pull_mint_stats():
     lamports_in_a_sol = int(1e9)
 
     if len(exchange_listings) > 0:
-        for exchange_listing_name, count in exchange_listings.items():
-            print(f"{count} Money Boys are listed on {exchange_listing_name}")
+        for exchange_listing_name, mints in exchange_listings.items():
+            print(f"{len(mints)} Money Boys are listed on {exchange_listing_name}")
+
+            # certaintly_listed = {
+            #     'FvBUCjPam99m13LgMez5TS8T1rmr3iidHgQQJYJcLQbW',
+            #     'eGxPk1GMw399MTqqtkQCCY8MvEEhm9oZZvK8Fr8Qton',
+            #     '7A9twRaBdeLL2FrPZgi3T7fEQob5q2Ypf3ZVEvgXLaoE',
+            #     'HWL65ZQcbJU8uWVcW86hPPFb72qErHJdoDv6AKuTGnQq',
+            #     '6YZkUNm71fyFtxwjQiRt6qX1NWWCn9EMPEzV121mbHwr',
+            #     '7jJUaPZojMV8g2FNejud69FEjSUQwHCV88GAn1QeSRcE',
+            #     '5Dx7r2As19EpDKWBpn38Xg8G3Y6CewiFsEu28NXTHGnE',
+            #     '5iLHqR8zymvknLK2WxVqW8gGrDmamGMpX9tuVgombjR9',
+            #     'Das4hWFZRTzWSZjqHH9eysJcVLpG7Cx8SAjeDNXNZ9u5',
+            #     '55FSKjyzneApkD31SbYBF28tiBAiK9n2Sjs3U3saYgQo',
+            #     '8jNsSbBzrjqbJLza2mCkgkF97UYQwwRdzBHC25PFqA7Q',
+            #     'JBXR7SrFbyipFwSvNog9nYAEoNyQLwARXZLwtyXsaVrG',
+            #     '5NMrvtwjXVG6UhmDo1e5LmhqxWqWbedwk21EGhyzMNGd'
+            # }
+            #
+            # print(certaintly_listed.difference(mints))
+            # print(mints.difference(certaintly_listed))
 
         print(f"Highest Price: {round(max(prices) / lamports_in_a_sol, 4)} SOL")
         print(f"Floor Price: {round(min(prices) / lamports_in_a_sol, 4)} SOL")
@@ -125,4 +154,9 @@ def pull_mint_stats():
 
 if __name__ == '__main__':
     # pull_mint_addresses()
+    # result = BLOCKCHAIN_API_RESOURCE.get_nft_listing(
+    #     mint_address='5Dx7r2As19EpDKWBpn38Xg8G3Y6CewiFsEu28NXTHGnE',
+    #     network=SolanaNetwork.MAINNET_BETA
+    # )
+    # print(result)
     pull_mint_stats()
