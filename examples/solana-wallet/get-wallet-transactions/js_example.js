4f6ab7f260faa4241a789d96e466ec8de3b74f61
Josh Wolff <jw1@stanford.edu>
2022-03-15 13:18:53 -0700

    Update

--- examples/solana-wallet/get-wallet-transactions/js_example.js
+++ examples/solana-wallet/get-wallet-transactions/js_example.js
@@ -10,39 +10,44 @@ APIKeyID.apiKey = 'API-KEY-ID';
 let APISecretKey = defaultClient.authentications['APISecretKey'];
 APISecretKey.apiKey = 'API-SECRET-KEY';
 
-// TODO:-
+let apiInstance = new theblockchainapi.SolanaWalletApi();
+let network = 'mainnet-beta'; // String | The network ID (devnet, mainnet-beta)
+let publicKey = 'GKNcUmNacSJo4S2Kq3DuYRYRGw3sNUfJ4tyqd198t6vQ'; // String | The public key of the account whose list of owned NFTs you want to get
 
-//let apiInstance = new theblockchainapi.SolanaWalletApi();
-//let network = 'mainnet-beta'; // String | The network ID (devnet, mainnet-beta)
-//let publicKey = 'GKNcUmNacSJo4S2Kq3DuYRYRGw3sNUfJ4tyqd198t6vQ'; // String | The public key of the account whose list of owned NFTs you want to get
-//
-//
-//let result = await apiInstance.solanaGetTokensBelongingToWallet(network, publicKey, {}).then((data) => {
-//  console.log('API called successfully.');
-//  return data;
-//}, (error) => {
-//  console.error(error);
-//  return error;
-//});
-//
-//console.log("Token Holdings: ", result);
-//
-//
-//// As you can see here... https://explorer.solana.com/address/BNEG1SxxPpcFHQszKshECqf9aRV5E2pmot7kLSnhzHrX/tokens
-//// ... public key owns 2 NFTs, but used to also own 2 others. Set `include_zero_balance_holdings` to `True` and
-//// `include_nfts` to `True` to see all 4 NFTs.. See that the balance is `0` for two of the NFTs and `1` for the other
-//// two NFTs.
-//publicKey = 'BNEG1SxxPpcFHQszKshECqf9aRV5E2pmot7kLSnhzHrX';
-//let opts = {
-//  'includeNfts': true, // Boolean | Whether or not to include NFTs in the response
-//  'includeZeroBalanceHoldings': true // Boolean | Whether or not to include holdings that have zero balance. This indicates that the wallet held this token or NFT in the past, but no longer holds it.
-//};
-//
-//result = await apiInstance.solanaGetTokensBelongingToWallet(network, publicKey, opts).then((data) => {
-//    console.log('API called successfully.');
-//    return data;
-//}, (error) => {
-//    console.error(error);
-//    return error;
-//});
-//console.log("Token Holdings: ", result);
+
+let result = await apiInstance.solanaGetWalletTransactions(network, publicKey).then((data) => {
+ console.log('API called successfully.');
+ return data;
+}, (error) => {
+ console.error(error);
+ return error;
+});
+
+console.log("Wallet Transactions: ", result);
+
+publicKey = 'BNEG1SxxPpcFHQszKshECqf9aRV5E2pmot7kLSnhzHrX';
+
+result = await apiInstance.solanaGetWalletTransactions(network, publicKey).then((data) => {
+   console.log('API called successfully.');
+   return data;
+}, (error) => {
+   console.error(error);
+   return error;
+});
+console.log("Wallet Transactions: ", result);
+
+
+// You can get more info based on the transaction signature like this:
+let txApiInstance = new theblockchainapi.SolanaTransactionApi();
+let txSignature = '5wHu1qwD7q5ifaN5nwdcDqNFo53GJqa7nLp2BeeEpcHCusb4GzARz4GjgzsEHMkBMgCJMGa6GSQ1VG96Exv8kt2W'; // String | The transaction signature of the transaction
+
+result = await txApiInstance.solanaGetTransaction(network, txSignature).then((data) => {
+  console.log('API called successfully.');
+  return data;
+}, (error) => {
+  console.error(error);
+  return null;
+});
+
+console.log("Transaction Details", result);
+
